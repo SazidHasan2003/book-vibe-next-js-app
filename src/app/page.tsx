e1f56ac203c830fb2c8491928e6e4be8d56b6d01
SazidHasan2003 <sarkersazid123@gmail.com>
2026-09-20 20:31:14 +0600

    creating the hero section by using daisy ui and adding file booksData.json in public folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import Hero from "./../components/homepage/Hero";
+
 const page = () => {
-  return <div>home page</div>;
+  return <Hero />;
 };
 
 export default page;
---
+++ src/components/homepage/Hero.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import heroImg from "../../assets/hero_img.jpg";
+
+const Hero = () => {
+  return (
+    <div className="hero bg-base-200 p-30 container mx-auto rounded-3xl">
+      <div className="hero-content flex-col lg:flex-row-reverse items-center justify-center">
+        <Image
+          alt="Hero Img"
+          src={heroImg}
+          className="max-w-sm rounded-lg shadow-2xl "
+        />
+        <div>
+          <h1 className="text-[56px] font-bold">
+            Books to freshen up your bookshelf
+          </h1>
+
+          <button className="btn btn-primary text-[#ffff] text-[20px] bg-[#23BE0A]">
+            View The List
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
