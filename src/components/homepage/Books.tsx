3c9e483475274dfd274301829e99ac8d6fb902d3
SazidHasan2003 <sarkersazid123@gmail.com>
2026-09-21 13:34:25 +0600

    fixing popular book acording to ratting

--- src/components/homepage/Books.tsx
+++ src/components/homepage/Books.tsx
@@ -12,13 +12,19 @@ const Books = async () => {
 
   return (
     <section className="container mx-auto my-16 px-4">
-      <h1 className="text-3xl font-bold text-center mb-8 font-serif">Books</h1>
+      <h1 className="text-3xl font-bold text-center mb-8 font-serif">
+        Explore Popular Books
+      </h1>
 
       {/* Grid wrapper ekbar e rakhben */}
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
-        {bookData.map((book: Ibook) => (
-          <BookCard key={book.bookId || book.bookId} bookProp={book} />
-        ))}
+        {bookData
+          ?.slice() // preventing manopolate orginal array
+          .sort((a: Ibook, b: Ibook) => b.rating - a.rating)
+          .slice(0, 6)
+          .map((book: Ibook) => (
+            <BookCard key={book.bookId} bookProp={book} />
+          ))}
       </div>
     </section>
   );
