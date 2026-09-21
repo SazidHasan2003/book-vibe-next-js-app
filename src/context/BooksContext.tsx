1ec8b859c248377a98fa8fd1ae72b043ef68f7e1
SazidHasan2003 <sarkersazid123@gmail.com>
2026-09-21 21:19:50 +0600

    creating a contex api using a folder named context and BooksContext.tsx

---
+++ src/context/BooksContext.tsx
@@ -0,0 +1,23 @@
+"use cliente";
+
+import React, { createContext, ReactNode, useState } from "react";
+
+const BooksContext = createContext({});
+
+const BooksProvider = ({ children }: { children: ReactNode }) => {
+  const [readBooks, setReadBooks] = useState([]);
+  const [wishList, setWishList] = useState([]);
+
+  const sharedData = {
+    readBooks,
+    setReadBooks,
+    wishList,
+    setWishList,
+  };
+
+  return (
+    <BooksContext.Provider value={sharedData}>{children}</BooksContext.Provider>
+  );
+};
+
+export default BooksProvider;
